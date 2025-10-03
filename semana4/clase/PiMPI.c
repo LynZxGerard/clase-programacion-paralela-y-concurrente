8a586b0b515c340bd863c76d899bbe7d43d9e0a7
daniel-orozco <danielorozcobtt@gmail.com>
2025-10-02 19:15:16 -0600

    Agregando comentarios

--- semana4/clase/PiMPI.c
+++ semana4/clase/PiMPI.c
@@ -16,34 +16,59 @@
 int main(int argc, char *argv[])
 {
     int n, myid, numprocs, i;
-    double PI25DT = 3.141592653589793238462643;
+    double PI25DT = 3.141592653589793238462643; // Valor "real" de pi para comparar el error
     double mypi, pi, h, sum, x;
+
+    // Inicializa MPI y arranca todos los procesos en paralelo
     MPI_Init(&argc,&argv);
+
+    // Obtiene el número total de procesos que se lanzaron con mpirun -np X
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+    // Obtiene el identificador único de este proceso (0,1,2,...,numprocs-1)
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
     while (1) {
+        // El proceso 0 es el encargado de pedir al usuario el número de intervalos "n"
         if (myid == 0) {
             printf("Enter the number of intervals: (0 quits) ");
             scanf("%d",&n);
         }
+
+        // Todos los procesos reciben el valor de "n" desde el proceso 0
         MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // Si el usuario escribe 0, todos los procesos terminan
         if (n == 0)
             break;
         else {
+            // h es el tamaño de cada subintervalo (ancho de cada "rectángulo" en la integración)
             h   = 1.0 / (double) n;
             sum = 0.0;
+
+            // Cada proceso calcula una parte de la suma total:
+            // el proceso 0 arranca en i=1, el 1 en i=2, ..., el último en i=numprocs
+            // después cada proceso sigue saltando de numprocs en numprocs
             for (i = myid + 1; i <= n; i += numprocs) {
-                x = h * ((double)i - 0.5);
-                sum += (4.0 / (1.0 + x*x));
+                x = h * ((double)i - 0.5);             // Punto medio del subintervalo
+                sum += (4.0 / (1.0 + x*x));            // Fórmula de la función 4/(1+x^2)
             }
+
+            // Cada proceso obtiene su aproximación parcial de pi
             mypi = h * sum;
+
+            // Se suman todas las aproximaciones parciales en el proceso 0
             MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
                        MPI_COMM_WORLD);
+
+            // Solo el proceso 0 imprime el resultado final
             if (myid == 0)
                 printf("pi is approximately %.16f, Error is %.16f\n",
                        pi, fabs(pi - PI25DT));
         }
     }
+
+    // Finaliza la ejecución de todos los procesos MPI
     MPI_Finalize();
     return 0;
 }
--- semana4/clase/SumaMPI.c
+++ semana4/clase/SumaMPI.c
@@ -17,20 +17,34 @@ void main(int argc, char** argv){
 
     int my_rank, s=0, x=0;
 
+    // Inicializa el entorno MPI
     MPI_Init(&argc, &argv);
-    //MPI_Init(NULL, NULL);
+
+    // Cada proceso obtiene su identificador único (rank)
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    if(my_rank==0) x=2;
+    // Solo el proceso 0 define el valor inicial de x
+    // En este caso, elegimos 2 para que todos los procesos lo usen
+    // Ese 2 representa los multiplos que se acumulan segun los n procesos del usuario
+    if(my_rank == 0) x = 2;
 
+    // Se "difunde" el valor de x a todos los procesos
+    // Esto funciona como los broadcast que aprendimos en redes
+    // Ahora todos los procesos tienen x = 2
     MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+    // Cada proceso multiplica x por su rank
+    // Esto crea valores distintos en cada proceso dependiendo de su número
     x *= my_rank;
 
+    // Se suman los valores de x de todos los procesos y se envía el resultado al proceso 0
+    // MPI_Reduce se encarga de recolectar y combinar todos los resultados
     MPI_Reduce(&x, &s, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if(my_rank==0) printf("The sum is %d.\n", s);
+    // Solo el proceso 0 imprime el resultado final de la suma
+    if(my_rank == 0)
+        printf("The sum is %d.\n", s);
 
+    // Finaliza el entorno MPI, liberando recursos
     MPI_Finalize();
-
 }
